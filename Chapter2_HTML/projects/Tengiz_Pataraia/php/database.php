c0d31d6fa48aabdd7a2db6175142c482d9ce7479
temurchichua <temur.chichua@iliauni.edu.ge>
2020-09-25 15:19:25 +0400

    removed php

--- Chapter2_HTML/projects/Tengiz_Pataraia/php/database.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = new PDO("mysql:host=localhost;dbname=id14215805_pokermaster","id14215805_mistera","Zklmjase.619");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
--- Chapter2_HTML/projects/Tengiz_Pataraia/php/game.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<center><h1>Jengiza she pitalo</h1></center>
-</body>
-</html>
