948563d4b745a93d366ae822562ead523ba12db4
Alisa Sanakoeva <alisa.sanakoevi.1@iliauni.edu.ge>
2020-10-01 13:36:20 +0400

    განვაახლე კოდი, დავამატე რეგისტრაცია

--- Chapter6_Templates/projects/Alisa_Sanakoeva/app.py
+++ Chapter6_Templates/projects/Alisa_Sanakoeva/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
@@ -9,5 +10,16 @@ def home():
 @app.route('/about/<pet>')
 def about(pet):
     return render_template('about.html', pet=pet)
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+@app.route('/status')
+def status():
+    data = request.args
+    username = data.get("username")
+    return render_template('status.html', username=username)
+
 
 app.run(port=4000, debug=True)
