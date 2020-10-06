a49632549a4c828fbbc652c7e2c6572a43c7f728
Zura <70130285+papiashviliz@users.noreply.github.com>
2020-10-06 14:41:40 +0400

    დავამატე ფაილების ატვირთვის ფუნქცია და Flash

--- Chapter7_Forms/Zura_Papiashvili/app.py
+++ Chapter7_Forms/Zura_Papiashvili/app.py
@@ -1,23 +1,26 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for,flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField, DateField,TextAreaField,SelectField
+from wtforms import StringField, SubmitField, RadioField, DateField,TextAreaField,SelectField,FileField
 from wtforms.validators import DataRequired, Email, EqualTo
-from flask_mail import Mail, Message
+# from flask_mail import Mail, Message
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-mail = Mail(app)
+# mail = Mail(app)
 app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'papiashvil@gmail.com'
-app.config['MAIL_PASSWORD'] = 'fsdqudsdyqcvxrch'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
+# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# app.config['MAIL_PORT'] = 465
+# app.config['MAIL_USERNAME'] = 'papiashvil@gmail.com'
+# app.config['MAIL_PASSWORD'] = 'fsdqudsdyqcvxrch'
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_USE_SSL'] = True
+# mail = Mail(app)
 
 
 class LetterForm(FlaskForm):
+    file = FileField()
     letter_text = TextAreaField('Your Future letter', validators=[DataRequired()],
                               render_kw={"placeholder": "Dear future me,"})
     deliver_in = RadioField('Deliver In:',
@@ -32,14 +35,17 @@ class LetterForm(FlaskForm):
 def home():
     form = LetterForm()
     if form.validate_on_submit():
+        session['filename']= secure_filename(form.file.data.filename)
+        form.file.data.save('static/uploads/' + session['filename'])
         session["letter_text"] = form.letter_text.data
         session['deliver_in'] = form.deliver_in.data
         session['privacy'] = form.privacy.data
         session['mail_address'] = form.mail_address.data
-        msg = Message('Future me', sender='papiashvil@gmail.com', recipients=['papiashvil@gmail.com', session['mail_address'] ]) 
-        msg.body = session["letter_text"]
+        # msg = Message('Future me', sender='papiashvil@gmail.com', recipients=['papiashvil@gmail.com', session['mail_address'] ])
+        # msg.body = session["letter_text"]
 
-        mail.send(msg)
+        # mail.send(msg)
+        flash('Your letter was sent')
         return redirect(url_for('report'))
 
     return render_template('home.html', form=form)
