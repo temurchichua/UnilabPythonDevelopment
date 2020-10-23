16a4f2638dc2c9cd809a71ba2257c5f5090db669
temurchichua <temur.chichua@iliauni.edu.ge>
2020-10-23 19:03:28 +0400

    Database in Views

---
+++ Chapter8_Database/example/Relationship_Demo/populate.py
@@ -0,0 +1,9 @@
+from model import db, Teacher, Student, Books
+
+# # შევქმნათ ორი სტუდენტი
+# tengo = Student('Tengo')
+# mariami = Student('Mariami')
+
+# # დავამატოთ სტუდენტები ბაზაში
+# db.session.add_all([tengo, mariami])
+# db.session.commit()
