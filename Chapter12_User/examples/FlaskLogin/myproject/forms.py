9d17664ddbefddba9ecdb8f59cb8882c79a88b50
temurchichua <temur.chichua@iliauni.edu.ge>
2020-11-06 19:02:09 +0400

    user login/registration

--- Chapter12_User/examples/FlaskLogin/myproject/forms.py
+++ Chapter12_User/examples/FlaskLogin/myproject/forms.py
@@ -1,3 +1,27 @@
-LogIn Form
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from myproject.models import User
 
-Registration Form
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords Must Match!')])
+    pass_confirm = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register!')
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=self.email.data).first():
+            raise ValidationError('Email has been registered')
+
+    def validate_username(self, username):
+        if User.query.filter_by(username=self.username.data).first():
+            raise ValidationError('Username has been registered')
